f68bc22a50f20bb86099f251ec2bbfa4e7f25ffe
AH-Ratul <ahratul21@gmail.com>
2024-07-14 01:23:07 +0600

    new icon added

--- src/data/all-icons.jsx
+++ src/data/all-icons.jsx
@@ -9,17 +9,19 @@ import {
   Person,
   Search,
   ShoppingCart,
+  ShoppingCartCheckout,
 } from "@mui/icons-material";
 
 export const allIcons = {
   home: <Home />,
   dashboard: <Dashboard />,
-  cart: <ShoppingCart fontSize="medium"/>,
+  cart: (size = "default") => <ShoppingCart fontSize={size} />,
   account: <Person fontSize="small" />,
   search: <Search />,
   list: <List fontSize="medium" />,
   wish: <Favorite />,
   menu: <Menu />,
-  expend: <ExpandMore fontSize="small"/>,
-  cross: <Close fontSize="large"/>,
+  expend: <ExpandMore fontSize="small" />,
+  cross: (size = "default") => <Close fontSize={size} />,
+  checkout: (size = "default") => <ShoppingCartCheckout fontSize={size} />,
 };
