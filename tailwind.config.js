c7adea62223f2f511e40106f4429ee10e85689ff
AH-Ratul <ahratul21@gmail.com>
2024-07-14 20:38:18 +0600

    animation and color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        'spin-slow': 'spin 1.5s linear infinite',  // Slow spin
+      },
       colors: {
-        primary: "#F9A03F",
+        primary: "#ff8c42",
+        secondary: '#F9A03F',
         babypowder: "#FFFFFA",
         gray1: "#7D7C7C",
         d1: "#232f3e",
