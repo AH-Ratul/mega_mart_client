39f7b55be669aa08a9fd9432b575c167481c5ca5
AH-Ratul <ahratul21@gmail.com>
2024-07-16 01:40:20 +0600

    home page modified

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,46 @@
 import React from "react";
-import p2 from "../assets/p2.jpg";
+import p1 from "../assets/banner2.svg";
+import p2 from "../assets/banner.svg";
+import p3 from "../assets/banner5.svg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Autoplay, Pagination } from "swiper/modules";
 
 const HomePage = () => {
   return (
     <main className="mt-4 flex justify-center items-center">
-      <div className="">
-        <img src={p2} alt="banner" className="w-96 h-[190px] object-cover rounded-md" />
-      </div>
+      <Swiper
+        direction="horizontal"
+        spaceBetween={30}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
+        modules={[Pagination, Autoplay]}
+      >
+        <SwiperSlide className="px-3 md:px-9">
+          <img
+            src={p1}
+            alt="banner"
+            className="w-full h-[170px]  md:h-[390px] object-cover rounded-md"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="px-3 md:px-9">
+          <img
+            src={p2}
+            alt="banner"
+            className="w-full h-[170px]  md:h-[390px] object-cover rounded-md"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="px-3 md:px-9">
+          <img
+            src={p3}
+            alt="banner"
+            className="w-full h-[170px] md:h-[390px] object-cover rounded-md"
+          />
+        </SwiperSlide>
+      </Swiper>
     </main>
   );
 };
