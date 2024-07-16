8551b597dd4b341d314dca6b959c5d7e00e9a75b
AH-Ratul <ahratul21@gmail.com>
2024-07-16 21:36:45 +0600

    layout modified

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeaderSm from "../components/Header/Header_sm";
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header/Header";
 
 const Main = () => {
   return (
     <main className="relative bg-babypowder">
       {/* Header section */}
+      <Header />
       <HeaderSm />
 
       {/** */}
