a6c3e27e3c7765f860d630f2a6dada9d83057c9a
AH-Ratul <ahratul21@gmail.com>
2024-07-16 21:42:45 +0600

    category component added

---
+++ src/components/Shared/Category/Category.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { allIcons } from "../../../data/all-icons";
+
+const Category = () => {
+  const { list } = allIcons;
+  return (
+    <div className="ml-9 w-[300px] bg-babypowder">
+      <h1 className="flex items-center ps-4 text-center text-xl bg-primary text-white font-bold py-2 rounded-t-lg">
+        <span className="text-3xl pr-4">{list}</span> CATEGORIES
+      </h1>
+      <div className=" flex flex-col lg:text-base font-thin font-serif h-[345px] border border-t-0 text-gray1 rounded-b-md overflow-auto custom-scrollbar">
+        <Link className="hover:text-primary hover:bg-gray-100 px-7 py-2 transition duration-500 ease-in-out">
+          Mobile & Accessories
+        </Link>
+        <Link className="hover:text-primary hover:bg-gray-100 px-7 py-2 transition duration-500 ease-in-out">
+          Electrical & Applience
+        </Link>
+        <Link className="hover:text-primary hover:bg-gray-100 px-7 py-2 transition duration-500 ease-in-out">
+          Home Decor & Life Style
+        </Link>
+        <Link className="hover:text-primary hover:bg-gray-100 px-7 py-2 transition duration-500 ease-in-out">
+          Women's Fashion
+        </Link>
+        <Link className="hover:text-primary hover:bg-gray-100 px-7 py-2 transition duration-500 ease-in-out">
+          Men's Fashion
+        </Link>
+        <Link className="hover:text-primary hover:bg-gray-100 px-7 py-2 transition duration-500 ease-in-out">
+          Computers & Accessories
+        </Link>
+        <Link className="hover:text-primary hover:bg-gray-100 px-7 py-2 transition duration-500 ease-in-out">
+          TV & Home Applience
+        </Link>
+        <Link className="hover:text-primary hover:bg-gray-100 px-7 py-2 transition duration-500 ease-in-out">
+          Gift's Items
+        </Link>
+        <Link className="hover:text-primary hover:bg-gray-100 px-7 py-2 transition duration-500 ease-in-out">
+          Grocery
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
