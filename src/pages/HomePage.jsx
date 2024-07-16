a52bb102b2c646d0f126087c6cba6ca0a4c98658
AH-Ratul <ahratul21@gmail.com>
2024-07-16 21:38:45 +0600

    homepage update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,46 +1,20 @@
 import React from "react";
-import p1 from "../assets/banner2.svg";
-import p2 from "../assets/banner.svg";
-import p3 from "../assets/banner5.svg";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { Autoplay, Pagination } from "swiper/modules";
+import Slider from "../components/Slider/Slider";
+import Category from "../components/Shared/Category/Category";
 
 const HomePage = () => {
   return (
-    <main className="mt-4 flex justify-center items-center">
-      <Swiper
-        direction="horizontal"
-        spaceBetween={30}
-        slidesPerView={1}
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 5000, disableOnInteraction: false }}
-        modules={[Pagination, Autoplay]}
-      >
-        <SwiperSlide className="px-3 md:px-9">
-          <img
-            src={p1}
-            alt="banner"
-            className="w-full h-[170px]  md:h-[390px] object-cover rounded-md"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="px-3 md:px-9">
-          <img
-            src={p2}
-            alt="banner"
-            className="w-full h-[170px]  md:h-[390px] object-cover rounded-md"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="px-3 md:px-9">
-          <img
-            src={p3}
-            alt="banner"
-            className="w-full h-[170px] md:h-[390px] object-cover rounded-md"
-          />
-        </SwiperSlide>
-      </Swiper>
+    <main className="mt-4 lg:mt-2 flex justify-center items-center">
+      <div className="lg:flex lg:items-center ">
+        {/* category */}
+        <div className="hidden lg:block">
+          <Category />
+        </div>
+        {/* slider */}
+        <div className="w-[410px] md:w-[770px] lg:w-[980px]">
+          <Slider />
+        </div>
+      </div>
     </main>
   );
 };
