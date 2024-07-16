67e580056ab4b504efbb98553568d11b09fdff47
AH-Ratul <ahratul21@gmail.com>
2024-07-16 23:21:39 +0600

    homepage updates

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import Slider from "../components/Slider/Slider";
 import Category from "../components/Shared/Category/Category";
+import Spotlights from "../components/spotlights/Spotlights";
+import cart1 from '../assets/img-cat-1.png';
 
 const HomePage = () => {
   return (
-    <main className="mt-4 lg:mt-2 flex justify-center items-center">
+    <main className="mt-4 lg:mt-2 flex flex-col justify-center items-center">
       <div className="lg:flex lg:items-center ">
         {/* category */}
         <div className="hidden lg:block">
           <Category />
         </div>
         {/* slider */}
-        <div className="w-[410px] md:w-[770px] lg:w-[980px]">
+        <div className="w-[410px] md:w-[750px] lg:w-[980px]">
           <Slider />
         </div>
+      </div>
+      <div className="mt-9 ">
+        <Spotlights />
+      </div>
+      <div>
+        <img src={cart1} alt="cart1" className="w-full" />
       </div>
     </main>
   );
