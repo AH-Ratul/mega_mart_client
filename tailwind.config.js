67dd2d9b0cfde60854261f127585c581d54a0ae3
AH-Ratul <ahratul21@gmail.com>
2024-07-16 21:39:24 +0600

    color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,16 @@ export default {
   theme: {
     extend: {
       animation: {
-        'spin-slow': 'spin 1.5s linear infinite',  // Slow spin
+        "spin-slow": "spin 1.5s linear infinite", // Slow spin
       },
       colors: {
         primary: "#ff8c42",
-        secondary: '#F9A03F',
+        secondary: "#F9A03F",
         babypowder: "#FFFFFA",
         gray1: "#7D7C7C",
+        gray2: "#67696c",
         d1: "#232f3e",
+        d2: "#4f5053",
       },
     },
   },
