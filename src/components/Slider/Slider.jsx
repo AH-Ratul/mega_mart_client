ea35c9a793fcc224f8742fab2fd3731d08b903f8
AH-Ratul <ahratul21@gmail.com>
2024-07-16 21:43:24 +0600

    slider added

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import p1 from "../../assets/bnn.svg";
+import p2 from "../../assets/banner.svg";
+import p3 from "../../assets/banner5.svg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Autoplay, Pagination } from "swiper/modules";
+
+const Slider = () => {
+  return (
+    <div>
+      <Swiper
+        direction="horizontal"
+        spaceBetween={30}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 5000, disableOnInteraction: false }}
+        modules={[Pagination, Autoplay]}
+      >
+        <SwiperSlide className="px-3 md:px-9 ">
+          <img
+            src={p1}
+            alt="banner"
+            className="w-full h-[170px]  md:h-[390px] object-cover rounded-md lg:rounded-none"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="px-3 md:px-9 ">
+          <img
+            src={p1}
+            alt="banner"
+            className="w-full h-[170px] md:h-[390px] object-cover rounded-md lg:rounded-none"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="px-3 md:px-9">
+          <img
+            src={p1}
+            alt="banner"
+            className="w-full h-[170px] md:h-[390px] object-cover rounded-md lg:rounded-none"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Slider;
