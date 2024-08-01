4bfd4c0dc174c63be0e0ac29e4b3e20abb412804
AH-Ratul <ahratul21@gmail.com>
2024-08-01 11:29:51 +0600

    updated smooth transition

--- src/components/Shared/DropDown/DropDown.jsx
+++ src/components/Shared/DropDown/DropDown.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import img from "../../../assets/p1.jpg";
+import { allIcons } from "../../../data/all-icons";
 
-const DropDown = () => {
-    return (
-        <div className='absolute'>
-            
-        </div>
-    );
+const DropDown = ({ isOpen, close }) => {
+    const {cross} = allIcons;
+  return (
+    <div
+      className={`${
+        isOpen ? "translate-x-0 right-3 shadow-a3" : "translate-x-full right-0"
+      } fixed bg-white text-gray1 mt-4 left-[900px] h-[460px]  border border-t-4 border-t-secondary z-10 transition duration-500 ease-out transform`}
+    >
+      <div className="flex justify-between px-3 mt-2">
+        <h1>cart</h1>
+        <span onClick={close} className="cursor-pointer text-2xl">
+          {cross}
+        </span>
+      </div>
+      <div className="p-3 mt-5">
+        <img src={img} alt="" className="w-12" />
+      </div>
+    </div>
+  );
 };
 
 export default DropDown;
