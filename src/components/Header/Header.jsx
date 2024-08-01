737313dfe5cc9ecc6f9fec51890b6e40ef5dca99
AH-Ratul <ahratul21@gmail.com>
2024-08-02 00:14:06 +0600

    new updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import logo from "../../../public/logo12.svg";
+import logo from "../../../public/logo123.svg";
 import { allIcons } from "../../data/all-icons";
 import Headlines from "../Headlines/Headlines";
-import DropOnHover from "../Shared/DropDown/DropOnHover";
 import DropDown from "../Shared/DropDown/DropDown";
 import useToggle from "../../hooks/useToggle";
+import { Link } from "react-router-dom";
+import bonus from "../../assets/bonus.png";
 
 const Header = () => {
-  const { account, wish, search, cart2 } = allIcons;
+  const { account, wish, search, cart3, lock } = allIcons;
   const [toggleStates, toggle] = useToggle({
     drop: false,
   });
@@ -19,49 +20,39 @@ const Header = () => {
     "Limited time offer: Buy 1 Get 1 Free! .....",
   ];
   return (
-    <header className="hidden lg:block ">
+    <header className="hidden lg:block bg-d1">
       {/* upper div */}
-      <div className="flex justify-between items-center bg-d2  px-10 border-b">
+      <div className="flex justify-between items-center px-10 py-1 2xl:py-2 border-b border-b-white/10">
         <div>
           <Headlines headlines={headlines} />
         </div>
-        {/* account */}
-        <div className="flex items-center text-white/70 ">
+        <div className="flex items-center text-white/70 text-xs">
           {/* my account */}
           <div className="group relative">
-            <span className="flex items-center text-xs px-3 py-2 border-r border-l border-gray1 gap-1 cursor-pointer">
+            <Link className="flex items-center mx-3 gap-1 hover:text-white/90">
               {account}MY ACCOUNT
-            </span>
-            <DropOnHover />
+            </Link>
           </div>
           {/* wish list */}
-          <div className="flex items-center text-xs px-3 pr-5 py-2  gap-1 cursor-pointer">
+          <Link className="flex items-center mx-3 gap-1 hover:text-white/90">
             {wish} WISH LIST (2)
-          </div>
-          {/* my cart */}
-          <div className="relative">
-            <button
-              onClick={() => toggle("drop")}
-              className="flex items-center text-xs text-white/90 px-3 py-2 gap-1 bg-gray2"
-            >
-              <span className="text-lg">{cart2}</span> MY CART
-              <span className="bg-red-400 pb-[1.5px] text-white rounded-full px-[6px] text-xs">
-                2
-              </span>
-            </button>
-            <DropDown isOpen={toggleStates.drop} close={() => toggle("drop")} />
-          </div>
+          </Link>
+          {/* login */}
+          <Link to='/login' className="flex items-center ml-3 gap-1 hover:text-white/90">
+            {lock} LOGIN
+          </Link>
         </div>
       </div>
       {/* lower div */}
-      <div className="py-4 px-10 flex justify-between items-center">
+      <div className="py-3 px-10 flex justify-between items-center">
         <a href="/">
-          <img src={logo} alt="logo" className="w-44" />
+          <img src={logo} alt="logo" className="w-56" />
         </a>
+        {/* Search */}
         <div className="flex w-[600px]">
           <input
             type="text"
-            className="border-2 border-primary text-gray1 rounded-tl rounded-bl p-1 ps-3 w-full outline-none"
+            className=" text-gray1 rounded-tl rounded-bl py-2 ps-3 w-full outline-none"
             placeholder="Search"
           />
           <button
@@ -71,9 +62,26 @@ const Header = () => {
             {search}
           </button>
         </div>
-        <div className="flex items-center bg-gray1"></div>
+        {/* banner & cart */}
+        <div className="flex items-center gap-5">
+          {/* bonus banner */}
+          <div className="w-60">
+            <img src={bonus} alt="bonus" />
+          </div>
+          {/* My Cart */}
+          <div className="relative">
+            <button
+              onClick={() => toggle("drop")}
+              className="flex items-center text-xs text-white/90 gap-2"
+            >
+              <span className="text-2xl  border border-primary text-primary rounded-full p-[6px]">
+                {cart3}
+              </span>
+            </button>
+            <DropDown isOpen={toggleStates.drop} close={() => toggle("drop")} />
+          </div>
+        </div>
       </div>
-      <div className="flex items-center bg-primary opacity-95 py-2"></div>
     </header>
   );
 };
