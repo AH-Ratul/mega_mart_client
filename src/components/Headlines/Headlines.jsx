6a8b95a3ba21f4613d2c700c538c30aaa4d4d810
AH-Ratul <ahratul21@gmail.com>
2024-08-01 17:43:19 +0600

    minor change

--- src/components/Headlines/Headlines.jsx
+++ src/components/Headlines/Headlines.jsx
@@ -3,7 +3,7 @@ import './headlines.css';
 
 const Headlines = ({headlines}) => {
     return (
-        <div className='relative overflow-hidden  p-1 w-64'>
+        <div className='relative overflow-hidden w-64'>
             <div className='headline-ticker inline-flex whitespace-nowrap'>
                 {[...headlines, ...headlines].map ((headline, index) => (
                     <span key={index} className='inline-block pr-1 text-sm text-primary'>
