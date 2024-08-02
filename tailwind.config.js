268f718d786183f049c05d8c1dd17ef7daa9f9b7
AH-Ratul <ahratul21@gmail.com>
2024-08-02 16:40:11 +0600

    new font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+      },
       animation: {
         "spin-slow": "spin 1.5s linear infinite", // Slow spin
       },
