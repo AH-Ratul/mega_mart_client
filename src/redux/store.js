bf87f9a72575117fcbcb62625aa8f9841bcfca51
AH-Ratul <ahratul21@gmail.com>
2024-08-23 23:43:08 +0600

    add reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userApi } from "./api/users_api";
+import authReducers from "./slices/authSlice";
 
 export const store = configureStore({
   reducer: {
+    auth: authReducers,
     [userApi.reducerPath]: userApi.reducer,
   },
 
