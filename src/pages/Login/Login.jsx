27289b694836d33ee44ed4d23f7d79f3dcbc3ccf
AH-Ratul <ahratul21@gmail.com>
2024-08-23 23:57:37 +0600

    login credentials updates

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { allIcons } from "../../data/all-icons";
+import { useLoginMutation } from "../../redux/api/users_api";
+import { useDispatch } from "react-redux";
+import { login, setLoading } from "../../redux/slices/authSlice";
 
 const Login = () => {
   const { google, phone } = allIcons;
+  const navigate = useNavigate();
 
   const {
     register,
@@ -13,15 +17,45 @@ const Login = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const [loginUser, { isSuccess, isLoading, isError, error }] =
+    useLoginMutation();
 
-    // clear fields
-    reset();
+  const dispatch = useDispatch();
+
+  // HANDLE LOGIN FUNCTIONALTIY
+  const onSubmit = async (data) => {
+    dispatch(setLoading(true));
+    try {
+      const result = await loginUser(data).unwrap();
+
+      console.log(result);
+
+      const user = {
+        name: result.data.user.name,
+        email: result.data.user.email,
+        phone: result.data.user.phone,
+      };
+      //console.log(user);
+      dispatch(login({ user: user, token: result.token }));
+
+      // clear fields
+      reset();
+
+      navigate("/");
+      dispatch(setLoading(false));
+    } catch (error) {
+      console.log("Err ->", error);
+      dispatch(setLoading(false));
+    }
+
+    // if (isLoading) {
+    //   return <Modal modal={<Loader />} />;
+    // }
   };
   return (
     <div className="flex justify-center items-center md:mt-6">
-      <div className="w-full px-5 md:px-20  lg:w-[500px] lg:px-16 py-4 rounded-lg lg:shadow-a2">
+      <div className="w-full px-4 md:px-20  lg:w-[500px] lg:px-12 py-4 rounded-lg lg:shadow-a2">
+        {isError && <p>{error.data.message}</p>}
         <h1 className="font-bold text-2xl text-center text-primary mt-9">
           Login Here
         </h1>
@@ -37,7 +71,9 @@ const Login = () => {
               },
             })}
             placeholder="Your Email *"
-            className=" border border-primary outline-none rounded-md ps-3 py-2 mt-5"
+            className={`${
+              errors.email && "border-red-500"
+            } border border-primary outline-none rounded-md ps-3 py-2 mt-5`}
           />
           {errors.email && (
             <p className="text-red-600 text-xs mt-1">{errors.email.message}</p>
@@ -65,17 +101,17 @@ const Login = () => {
               Forgot Password?
             </Link>
           </div>
-          <button className="mt-3 bg-secondary hover:opacity-90 py-3 rounded-md text-white text-base font-medium tracking-wider">
-            LOGIN
+          <button className="mt-3 bg-secondary hover:opacity-90 py-3 rounded-md text-white text-base font-medium tracking-wider text-center">
+            {isLoading ? "Logging in..." : "LOGIN"}
           </button>
         </form>
         {/* social login */}
         <div className="flex justify-center items-center gap-5 mt-5">
-          <button className="flex justify-center items-center  rounded-md gap-2 py-1 text-xl w-full border border-secondary ">
+          <button className="flex justify-center items-center  rounded-md gap-2 py-2 text-xl w-full border border-secondary ">
             <span>{google}</span>
             <span className="text-primary text-base">Google</span>
           </button>
-          <Link className="flex justify-center items-center rounded-md gap-2 py-1 text-xl w-full border border-secondary ">
+          <Link className="flex justify-center items-center rounded-md gap-2 py-2 text-xl w-full border border-secondary ">
             <span>{phone}</span>
             <span className="text-primary text-base">Phone</span>
           </Link>
