fd84d9ac8f2646a374fadd8ce602901f9d3e5f71
AH-Ratul <ahratul21@gmail.com>
2024-08-24 23:07:22 +0600

    toaster added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import HeaderSm from "../components/Header/Header_sm";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
+import { Toaster } from "react-hot-toast";
 
 const Main = () => {
   return (
@@ -12,6 +13,7 @@ const Main = () => {
 
       {/** */}
       <Outlet />
+      <Toaster position="top-right"/>
     </main>
   );
 };
