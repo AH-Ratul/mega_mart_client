4ba45314e3727e9ef1fe2889e2dab2a91180fd79
AH-Ratul <ahratul21@gmail.com>
2024-08-25 12:05:21 +0600

    new route added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,26 +3,29 @@ import Main from "../Layout/Main";
 import HomePage from "../pages/HomePage";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
-
-
+import MyAccount from "../components/Profile/MyAccount";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main />,
-        children: [
-            {
-                path: '/',
-                element: <HomePage />
-            },
-            {
-                path: 'login',
-                element: <Login />
-            },
-            {
-                path: 'register',
-                element: <Register />
-            },
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "myaccount",
+        element: <MyAccount />,
+      },
+    ],
+  },
+]);
