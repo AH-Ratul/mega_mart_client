5bb0bbd136b3d35b44dee887a16beafc6edf1902
AH-Ratul <ahratul21@gmail.com>
2024-08-26 00:57:31 +0600

    width increased

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,14 +5,14 @@ import Spotlights from "../components/spotlights/Spotlights";
 
 const HomePage = () => {
   return (
-    <main className="mt-4 lg:mt-2 flex flex-col justify-center items-center">
+    <main className="mt-4 flex flex-col justify-center items-center">
       <section className="lg:flex lg:items-center lg:gap-5">
         {/* category */}
         <div className="hidden lg:block">
           <Category />
         </div>
         {/* slider */}
-        <div className="w-[410px] md:w-[770px] lg:w-[950px]">
+        <div className="w-[410px] md:w-[770px] lg:w-[999px]">
           <Slider />
         </div>
       </section>
