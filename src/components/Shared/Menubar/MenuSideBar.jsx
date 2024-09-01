e0359691810daa67864a45331585869f3f7c5248
AH-Ratul <ahratul21@gmail.com>
2024-09-01 18:00:39 +0600

    design updates

--- src/components/Shared/Menubar/MenuSideBar.jsx
+++ src/components/Shared/Menubar/MenuSideBar.jsx
@@ -1,35 +1,67 @@
 import React from "react";
 import logo from "../../../../public/logo12.svg";
 import { allIcons } from "../../../data/all-icons";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+import { useDispatch } from "react-redux";
+import { logOut } from "../../../redux/slices/authSlice";
 
 const MenuSideBar = ({ isOpen, close }) => {
   const { cross } = allIcons;
 
+  const { user } = useAuth();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  //console.log(user);
+
+  const handleLogOut = () => {
+    dispatch(logOut());
+    navigate("/");
+  };
+
   return (
-    <div
-      className={`fixed h-[100dvh] left-0 right-8 text-gray1 bg-white z-20 top-0 px-3 overflow-hidden inset-y-0 transform ${
-        isOpen ? "translate-x-0 shadow-a2" : "-translate-x-full"
-      } transition-transform duration-500 ease-in-out`}
-    >
-      <div className="flex justify-between items-center pl-4 mt-2">
-        <img src={logo} alt="logo" className="w-32 md:w-52 py-2" />
-        <span
-          onClick={close}
-          className="hover:text-primary text-black text-3xl"
-        >
-          {cross}
-        </span>
+    <div>
+      <div
+        className={`fixed h-[100dvh] left-0 right-32 md:right-64 text-gray1 bg-white z-20 top-0 px-3 overflow-hidden inset-y-0 transform ${
+          isOpen ? "translate-x-0 shadow-a2" : "-translate-x-full shadow-a2"
+        } transition-transform duration-500 ease-in-out`}
+      >
+        <div className="flex justify-between items-center pl-4 mt-2">
+          <img src={logo} alt="logo" className="w-32 md:w-52 py-2" />
+          <span
+            onClick={close}
+            className="hover:text-primary text-black text-3xl"
+          >
+            {cross}
+          </span>
+        </div>
+        <div className=" mt-4 pl-5 md:pl-6 flex flex-col gap-2 text-lg md:text-xl">
+          <Link to="/account" onClick={close} className="hover:text-primary">
+            My Account
+          </Link>
+          <Link className="hover:text-primary">My Oreders</Link>
+          <Link className="hover:text-primary">WishList</Link>
+          {user ? (
+            <button
+              onClick={handleLogOut}
+              className="hover:text-primary text-start"
+            >
+              Logout
+            </button>
+          ) : (
+            <Link to="/login" onClick={close} className="hover:text-primary">
+              Login
+            </Link>
+          )}
+        </div>
       </div>
-      <div className=" mt-4 pl-8 md:pl-10 flex flex-col gap-2 text-lg md:text-xl">
-        <Link className="hover:text-primary">My Account</Link>
-        <Link className="hover:text-primary">Products</Link>
-        <Link className="hover:text-primary">Contacts</Link>
-        <Link to="/login" onClick={close} className="hover:text-primary">
-          Login
-        </Link>
-        
-      </div>
+      <div
+        onClick={close}
+        className={`fixed w-screen h-screen bg-gray-900 opacity-0 top-0 left-0 z-10 transform ${
+          isOpen ? "translate-x-0 shadow-2xl" : "-translate-x-full"
+        } transition-transform duration-700 ease-in-out`}
+      ></div>
     </div>
   );
 };
