6a184dbdbcb7fc721e2ad743727bf1ebb9e78ab5
AH-Ratul <ahratul21@gmail.com>
2024-09-02 15:57:16 +0600

    design updates

--- src/components/Shared/Cartbar/CartSideBar.jsx
+++ src/components/Shared/Cartbar/CartSideBar.jsx
@@ -5,48 +5,47 @@ import p1 from "../../../assets/p1.jpg";
 const CartSideBar = ({ isOpen, close }) => {
   const { cross, cart, checkout } = allIcons;
   return (
-    <div
-      className={`fixed h-[100dvh]  left-0 right-8 text-gray1 bg-white z-20 top-0 px-4 py-3 md:px-7 md:py-4 overflow-hidden inset-y-0 transform ${
-        isOpen ? "translate-x-0 shadow-2xl" : "-translate-x-full"
-      } transition-transform duration-500 ease-in-out`}
-    >
-      <div className="flex justify-between items-center border-b-2 pb-4 border-primary">
-        <h1 className="text-2xl font-bold text-orange-500">My Cart</h1>
-        <span
-          onClick={close}
-          className="hover:text-primary text-black text-3xl"
-        >
-          {cross}
-        </span>
-      </div>
-      <div className="mt-5 text-black flex items-center gap-7">
-        <img src={p1} alt="" className="w-12 md:w-20" />
-        <p>Dell Laptop i5 8th gen.. </p>
-        <p className="text-lg">$3000</p>
-        <span className="text-base bg-gray1 text-white  px-1">{cross}</span>
-      </div>
-      <div className="border-t-2 border-b-2 border-black/40 pb-2 mt-12 text-black">
-        <div className="flex justify-between items-center px-3 mt-2">
-          <p>Sub-Total</p>
-          <p>$3000</p>
+    <div>
+      <div
+        className={`fixed h-[100dvh]  left-0 right-32 md:right-64 text-gray1 bg-white z-20 top-0 px-4 py-3 md:px-7 md:py-4 overflow-hidden inset-y-0 transform ${
+          isOpen ? "translate-x-0 shadow-2xl" : "-translate-x-full"
+        } transition-transform duration-500 ease-in-out`}
+      >
+        <div className="flex justify-between items-center ">
+          <h1 className="text-2xl font-bold text-orange-500">My Cart</h1>
+          <span
+            onClick={close}
+            className="hover:text-primary text-black text-3xl"
+          >
+            {cross}
+          </span>
         </div>
-        <div className="flex justify-between items-center px-3 mt-2">
-          <p>Vat(15%)</p>
-          <p>$15</p>
+
+        {/* Cart items */}
+        <div className="h-[75%] md:h-[85%] mt-9 overflow-auto custom-scrollbar">
+          <div className=" text-black flex justify-between items-center p-1">
+            <img src={p1} alt="" className="w-10 md:w-16" />
+            <p className="text-xs md:text-sm -ml-7 md:-ml-36">
+              Dell Laptop i5 8th gen..{" "}
+            </p>
+            <span className="text-base  bg-gray1 text-white  px-1">
+              {cross}
+            </span>
+          </div>
         </div>
-        <div className="flex justify-between items-center px-3 mt-2">
-          <p>Total</p>
-          <p>$3015</p>
+
+        <div className="flex justify-center items-center mt-7">
+          <button className="bg-primary w-full text-center text-white/90  py-2 flex justify-center items-center gap-1 rounded-md">
+            {cart} View Cart
+          </button>
         </div>
       </div>
-      <div className="flex justify-center items-center gap-20 mt-7">
-        <button className="bg-d1 text-white/90 px-3 py-2 hover:text-secondary flex items-center">
-          {cart} View Cart
-        </button>
-        <button className="bg-d1 text-white/90 px-3 py-2 hover:text-secondary flex items-center">
-          {checkout} Checkout
-        </button>
-      </div>
+      <div
+        onClick={close}
+        className={`fixed w-screen h-screen bg-gray-900 top-0 left-0 z-10 transform ${
+          isOpen ? "opacity-30 visible" : "opacity-0 invisible"
+        } transistion-opacity duration-500 ease-in-out`}
+      ></div>
     </div>
   );
 };
