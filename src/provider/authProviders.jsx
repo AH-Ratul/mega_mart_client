37cfae38f8f1baecfe2e3fb844b4d8dd43b321ca
AH-Ratul <ahratul21@gmail.com>
2024-09-03 22:27:20 +0600

    handle user data when login

--- src/provider/authProviders.jsx
+++ src/provider/authProviders.jsx
@@ -1,8 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useGetMeQuery } from "../redux/api/users_api";
+import Modal from "../components/Shared/Modal/Modal";
+import Loader from "../components/Shared/Loader/Loader";
+import { setUser } from "../redux/slices/authSlice";
+import { useEffect } from "react";
 
+const AuthProvider = ({ children }) => {
+  const dispatch = useDispatch();
+  const { data: userData, isLoading } = useGetMeQuery();
+  const { loading } = useSelector((state) => state.auth);
 
-const AuthProvider = () => {
+  useEffect(() => {
+    if (userData) {
+      const user = userData.data;
+      dispatch(setUser(user));
+    }
+  }, [userData, dispatch]);
 
-}
+  if (isLoading || loading) {
+    return <Modal modal={<Loader color="white" size="2xl" />} />;
+  }
 
+  return <>{children}</>;
+};
 
 export default AuthProvider;
