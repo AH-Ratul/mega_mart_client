2ef909fd03320af1eb7e95032af0e6f02fe6da73
AH-Ratul <ahratul21@gmail.com>
2024-09-03 22:07:38 +0600

    provider update

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import AuthProvider from "./provider/authProviders.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </Provider>
   </React.StrictMode>
 );
