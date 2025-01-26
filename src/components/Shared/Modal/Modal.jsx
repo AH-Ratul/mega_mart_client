67fd5f2534a31ecea6eec747d4a59da00299b7be
AH-Ratul <ahratul21@gmail.com>
2025-01-26 17:32:09 +0600

    update bg color

--- src/components/Shared/Modal/Modal.jsx
+++ src/components/Shared/Modal/Modal.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Modal = ({ modal }) => {
   return (
-    <main className="fixed h-[100dvh] bg-primary z-30 inset-0 flex justify-center items-center overflow-hidden">
+    <main className="fixed h-[100dvh] bg-white z-30 inset-0 flex justify-center items-center overflow-hidden">
       <div>{modal}</div>
     </main>
   );
