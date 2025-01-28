b6f5cd18cb3ef74f4ab27d6c96e8b454305a4d4e
AH-Ratul <ahratul21@gmail.com>
2025-01-28 18:01:33 +0600

    add store for product api

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userApi } from "./api/users_api";
 import authReducers from "./slices/authSlice";
+import productReducers from "./slices/productSlice";
+import { productApi } from "./api/products_api";
 
 export const store = configureStore({
   reducer: {
     auth: authReducers,
+    products: productReducers,
     [userApi.reducerPath]: userApi.reducer,
+    [productApi.reducerPath]: productApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware),
+    getDefaultMiddleware().concat(userApi.middleware, productApi.middleware),
 });
