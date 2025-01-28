ead00fc608d486a23c933d9eef4d69b84216c92f
AH-Ratul <ahratul21@gmail.com>
2025-01-28 22:09:35 +0600

    flexbox update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,13 +3,13 @@ import Products from "../components/Products/Products";
 
 const HomePage = () => {
   return (
-    <main className="relative flex flex-col justify-center items-center">
+    <main className="relative flex flex-col">
       <section className="lg:flex lg:items-center">
         <div className="bg-[#DF1238] w-screen h-96 flex justify-center pt-14"></div>
       </section>
 
-      <div className="mt-20 px-3">
-        <h1 className="text-xl ">Buy Your All Favorite Products</h1>
+      <div className="mt-20 px-3 flex flex-col justify-center items-center">
+        <h1 className="text-xl">Buy Your All Favorite Products</h1>
         <Products />
       </div>
     </main>
