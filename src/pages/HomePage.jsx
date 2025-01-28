cf3e5a4960932b1ee02c72389a288cf0580b8733
AH-Ratul <ahratul21@gmail.com>
2025-01-28 18:03:59 +0600

    add product section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import Products from "../components/Products/Products";
 
 const HomePage = () => {
   return (
     <main className="relative flex flex-col justify-center items-center">
       <section className="lg:flex lg:items-center">
-        <div className="bg-[#DF1238] w-screen h-96 flex justify-center pt-14">
-          
-        </div>
+        <div className="bg-[#DF1238] w-screen h-96 flex justify-center pt-14"></div>
       </section>
 
-      <div></div>
+      <div className="mt-20 px-3">
+        <h1 className="text-xl ">Buy Your All Favorite Products</h1>
+        <Products />
+      </div>
     </main>
   );
 };
