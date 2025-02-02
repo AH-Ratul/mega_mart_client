b58c6fd15c9f379430a3d3d6781880bdc237b0ee
AH-Ratul <ahratul21@gmail.com>
2025-02-02 23:02:01 +0600

    some updates

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useGetProductByIdQuery } from "../../redux/api/products_api";
 import { useSelector } from "react-redux";
+import Loader from "../../components/Shared/Loader/Loader";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -12,7 +13,7 @@ const ProductDetails = () => {
     productData?.data || [];
 
   if (isLoading && loading) {
-    return <p>Loading...</p>;
+    return <Loader size="30px" />;
   }
 
   if (error) {
@@ -20,15 +21,23 @@ const ProductDetails = () => {
   }
 
   return (
-    <div className="pt-24 flex justify-center items-center">
-      <div className="flex justify-start gap-20 h-[450px] w-full mx-5 lg:w-[960px] xl:w-[1240px] 2xl:w-[1290px]">
-        <div>
-          <img src={productImages} alt="img" className="w-96 md:h-96 " />
+    <div className="pt-16 flex justify-center items-center mb-32">
+      <div className="flex flex-col h-[450px] w-full lg:flex-row  lg:justify-start lg:w-[960px] xl:w-[1240px] 2xl:w-[1290px]">
+        <div className="flex justify-center items-center">
+          <img src={productImages} alt="img" className="w- h-80 " />
         </div>
-        <div className="">
+        <div className="mt-2 mx-3">
           <p>{productName}</p>
-          <p className="font-bold text-xl">&#2547; {price}</p>
-          <p className="text-xs w-96 text-pretty my-2">{description}</p>
+          <div className="flex justify-between items-center text-xs my-1">
+            <span>7 sold</span>
+            <span>4.3 ⭐⭐⭐⭐⭐</span>
+          </div>
+          <p className="font-bold text-lg">&#2547; {price}</p>
+          <div className="border rounded-md w-fit flex items-center">
+            <button className="bg-gray-100 text-2xl w-9">-</button>
+            <span className="mx-3">1</span>
+            <button className="bg-gray-100 text-2xl w-9">+</button>
+          </div>
         </div>
       </div>
     </div>
