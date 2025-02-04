f8d4d84bfc195835b1c1af112685c126d4c79c9a
AH-Ratul <ahratul21@gmail.com>
2025-02-04 20:07:27 +0600

    store quantity reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { userApi } from "./api/users_api";
 import authReducers from "./slices/authSlice";
 import productReducers from "./slices/productSlice";
 import { productApi } from "./api/products_api";
+import quantityReducer from "./slices/quantitySlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducers,
     products: productReducers,
+    quantity: quantityReducer,
     [userApi.reducerPath]: userApi.reducer,
     [productApi.reducerPath]: productApi.reducer,
   },
