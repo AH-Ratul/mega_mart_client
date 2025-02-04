ec97dcf2f9ad15d19aa9e63f50c29b0dc6e46733
AH-Ratul <ahratul21@gmail.com>
2025-02-04 20:06:54 +0600

    update some feature & handle quantity counter

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useGetProductByIdQuery } from "../../redux/api/products_api";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Loader from "../../components/Shared/Loader/Loader";
 import Products from "../../components/Products/Products";
+import { decrement, increment } from "../../redux/slices/quantitySlice";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -12,6 +13,9 @@ const ProductDetails = () => {
   console.log(id, productData);
   const { productName, productImages, price, discountPrice, description } =
     productData?.data || [];
+
+  const quantity = useSelector((state) => state.quantity.value);
+  const dispatch = useDispatch();
 
   if (isLoading && loading) {
     return <Loader size="30px" />;
@@ -23,8 +27,8 @@ const ProductDetails = () => {
 
   return (
     <>
-      <div className="pt-14 lg:pt-16 flex justify-center items-center mb-32">
-        <div className="flex flex-col lg:flex-row w-full max-w-7xl bg-whit overflow-hidden">
+      <div className="mt-[52px] lg:mt-[59px] flex justify-center items-center mb-32">
+        <div className="flex flex-col lg:flex-row w-full max-w-7xl overflow-hidden">
           {/* Product Image */}
           <div className="hidden lg:block w-20 h-20 border-2 border-black cursor-pointer mr-1">
             <img
@@ -33,24 +37,26 @@ const ProductDetails = () => {
               className="w-full h-full"
             />
           </div>
-          <div className="flex justify-center items-center h-[550px] bg-gray-100 p-4 lg:w-1/2">
+          <div className="flex justify-center items-center w-full lg:w-1/2 h-auto lg:h-[550px] overflow-hidden">
             <img
               src={productImages}
               alt={productName}
-              className=" w-full h-full object-contain rounded-md"
+              className="w-full h-full object-cover"
             />
           </div>
 
           {/* Product Details */}
-          <div className="flex flex-col justify-between px-6 lg:w-1/2">
+          <div className="flex flex-col justify-between mt-1 lg:mt-0 px-6 lg:w-1/2">
             <div>
-              <h1 className="mb-1 text-2xl">{productName}</h1>
+              <h1 className="mb-1 text-xl">{productName}</h1>
               <span className="bg-slate-200 px-1 py-[1px] text-sm font-medium tracking-wide rounded-md">
                 Brand: Walton
               </span>
               <div className="flex justify-start items-center gap-4 text-sm text-gray-500 mt-2 mb-4">
                 <span>0 sold</span>
-                <span className="flex items-center">⭐⭐⭐⭐⭐ 4.3 Ratings</span>
+                <span className="flex items-center">
+                  ⭐⭐⭐⭐⭐ 4.3 Ratings
+                </span>
               </div>
               <p className="font-semibold text-xl mb-4 flex items-center gap-2">
                 {discountPrice ? (
@@ -68,10 +74,28 @@ const ProductDetails = () => {
             </div>
 
             {/* Quantity Selector */}
-            <div className="flex items-center border rounded-md w-fit bg-gray-100 mt-2 lg:mt-0">
-              <button className="px-4 py-2 text-xl hover:bg-gray-200">-</button>
-              <span className="px-4 py-2 text-lg bg-white">1</span>
-              <button className="px-4 py-2 text-xl hover:bg-gray-200">+</button>
+            <div className="flex items-center gap-4 w-fit mt-2 lg:mt-0">
+              <span className="text-gray-700">Quantity: </span>
+              <div className="flex items-center bg-gray-100">
+                <button
+                  onClick={() => dispatch(decrement())}
+                  className={`px-4 text-xl transition ${
+                    quantity === 1 ? "cursor-not-allowed" : "hover:bg-gray-200"
+                  } `}
+                  disabled={quantity === 1}
+                >
+                  -
+                </button>
+                <span className="w-10 text-center py-[1px] text-lg bg-white">
+                  {quantity}
+                </span>
+                <button
+                  onClick={() => dispatch(increment())}
+                  className="px-4 text-xl hover:bg-gray-200"
+                >
+                  +
+                </button>
+              </div>
             </div>
 
             {/* Add to Cart & Buy Button */}
