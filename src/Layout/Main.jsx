616cf8f262317ebbd1a1c2ae5d534435abf7adbd
AH-Ratul <ahratul21@gmail.com>
2025-02-11 21:53:38 +0600

    remove font

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,16 +3,18 @@ import HeaderSm from "../components/Header/Header_sm";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
 import { Toaster } from "react-hot-toast";
+import Footer from "../components/Footer/Footer";
 
 const Main = () => {
   return (
-    <main className="relative font-Roboto">
+    <main className="relative">
       {/* Header section */}
       <Header />
       <HeaderSm />
 
       {/** */}
       <Outlet />
+      <Footer />
       <Toaster position="top-right" />
     </main>
   );
