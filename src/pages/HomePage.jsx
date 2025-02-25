6908c23de51ca21b39b3d8faab05cb68c0b3784f
AH-Ratul <ahratul21@gmail.com>
2025-02-25 21:59:14 +0600

    adjust the center position & width

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,10 +8,14 @@ const HomePage = () => {
         <div className="bg-[#DF1238] w-screen h-96 flex justify-center pt-14"></div>
       </section>
 
-      <div className="mt-20 px-3 flex flex-col justify-center items-center">
-        <h1 className="text-xl">Buy Your All Favorite Products</h1>
-        <Products />
-      </div>
+      <section className="mt-20 px-3 flex flex-col items-center w-full">
+        <div className="w-full flex flex-col max-w-[1270px]">
+          <h1 className="text-xl mb-14 ml-3">Buy Your All Favorite Products</h1>
+          <div className="flex justify-center">
+            <Products />
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
