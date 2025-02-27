2ca9c8df583044b6917bfa424a295f93070e13a1
AH-Ratul <ahratul21@gmail.com>
2025-02-27 22:52:21 +0600

    cart reducer added

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,14 @@ import authReducers from "./slices/authSlice";
 import productReducers from "./slices/productSlice";
 import { productApi } from "./api/products_api";
 import quantityReducer from "./slices/quantitySlice";
+import cartReducer from "./slices/cartSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducers,
     products: productReducers,
     quantity: quantityReducer,
+    cart: cartReducer,
     [userApi.reducerPath]: userApi.reducer,
     [productApi.reducerPath]: productApi.reducer,
   },
