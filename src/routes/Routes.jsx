7a4626147c61b57c80ba7c977c28867a515669e7
AH-Ratul <ahratul21@gmail.com>
2025-02-28 17:52:43 +0600

    wrapped routes with lazy loading

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,15 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
-import HomePage from "../pages/HomePage";
-import Login from "../pages/Login/Login";
-import Register from "../pages/Register/Register";
-import MyAccount from "../components/Profile/MyAccount";
 import PrivateRoute from "./PrivateRoute/PrivateRoute";
-import ForgetPassword from "../pages/ForgetPassword/ForgetPassword";
-import ResetPassword from "../pages/ResetPassword/ResetPassword";
-import VerifyCode from "../pages/VerifyCode/VerifyCode";
-import ProductDetails from "../pages/ProductDetails/ProductDetails";
-import CartPage from "../pages/Cart/CartPage";
+import { lazy, Suspense } from "react";
+import Modal from "../components/Shared/Modal/Modal";
+import Loader from "../components/Shared/Loader/Loader";
+
+// Lazy loaded pages
+const HomePage = lazy(() => import("../pages/HomePage"));
+const Login = lazy(() => import("../pages/Login/Login"));
+const Register = lazy(() => import("../pages/Register/Register"));
+const MyAccount = lazy(() => import("../components/Profile/MyAccount"));
+const ForgetPassword = lazy(() => import("../pages/ForgetPassword/ForgetPassword"));
+const ResetPassword = lazy(() => import("../pages/ResetPassword/ResetPassword"));
+const VerifyCode = lazy(() => import("../pages/VerifyCode/VerifyCode"));
+const ProductDetails = lazy(() => import("../pages/ProductDetails/ProductDetails"));
+const CartPage = lazy(() => import("../pages/Cart/CartPage"));
+
+//Suspense wrapper 
+const loadable =(Component) => (
+  <Suspense fallback={<Modal modal={<Loader size="40px" />}/>}>
+    <Component />
+  </Suspense>
+)
 
 export const router = createBrowserRouter([
   {
@@ -18,43 +30,43 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <HomePage />,
+        element: loadable(HomePage),
       },
       {
         path: "login",
-        element: <Login />,
+        element: loadable(Login),
       },
       {
         path: "register",
-        element: <Register />,
+        element: loadable(Register),
       },
       {
         path: "account",
         element: (
           <PrivateRoute>
-            <MyAccount />
+            {loadable(MyAccount)}
           </PrivateRoute>
         ),
       },
       {
         path: "forgetpassword",
-        element: <ForgetPassword />,
+        element: loadable(ForgetPassword),
       },
       {
         path: "verifyCode",
-        element: <VerifyCode />,
+        element: loadable(VerifyCode),
       },
       {
         path: "resetPassword",
-        element: <ResetPassword />,
+        element: loadable(ResetPassword),
       },
       {
         path: "details/:id/:name",
-        element: <ProductDetails />,
+        element: loadable(ProductDetails),
       },
       {
         path: "shopping_cart",
-        element: <CartPage />,
+        element: loadable(CartPage),
       },
     ],
   },
