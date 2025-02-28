96426c412d1d23ae68efe046ce4eb4919471ad19
AH-Ratul <ahratul21@gmail.com>
2025-02-28 19:48:49 +0600

    handle cart functionality

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Loader from "../../components/Shared/Loader/Loader";
 import Products from "../../components/Products/Products";
 import { decrement, increment } from "../../redux/slices/quantitySlice";
+import CustomToast from "../../hooks/CustomToast";
+import { addToCart } from "../../redux/slices/cartSlice";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -16,6 +18,12 @@ const ProductDetails = () => {
 
   const quantity = useSelector((state) => state.quantity.value);
   const dispatch = useDispatch();
+
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+
+    CustomToast({ type: "success", message: "Added to cart" });
+  };
 
   // Scrolls to the top when navigating to a new product detail page.
   useEffect(() => {
@@ -32,7 +40,7 @@ const ProductDetails = () => {
 
   return (
     <>
-      <div className="mt-[52px] lg:mt-[59px] flex justify-center items-center mb-32">
+      <div className="mt-[53px] lg:mt-[75px] flex justify-center items-center mb-32">
         <div className="flex flex-col lg:flex-row w-full max-w-7xl overflow-hidden">
           {/* Product Image */}
           <div className="hidden lg:block w-20 h-20 border-2 border-black cursor-pointer mr-1">
@@ -105,10 +113,13 @@ const ProductDetails = () => {
 
             {/* Add to Cart & Buy Button */}
             <div className="flex items-center gap-5 pb-1">
-              <button className="mt-6 w-1/2 border border-black/70 py-3 rounded-full transition-transform duration-300 hover:scale-105">
+              <button
+                onClick={() => handleAddToCart(productData.data)}
+                className="mt-6 w-1/2 border border-black/70 py-3 rounded-lg transition-transform duration-300 hover:scale-105"
+              >
                 Add to Cart
               </button>
-              <button className="mt-6 w-1/2 bg-primary text-white py-3 rounded-full transition-transform duration-300 hover:scale-105">
+              <button className="mt-6 w-1/2 bg-primary text-white py-3 rounded-lg transition-transform duration-300 hover:scale-105">
                 Buy Now
               </button>
             </div>
