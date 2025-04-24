7a61d1bddccf3da37667ebdf4691b246eaa11c21
AH-Ratul <ahratul21@gmail.com>
2025-04-24 19:26:12 +0600

    add breadcrumb & update some features

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -98,147 +98,168 @@ const Checkout = () => {
   }
 
   return (
-    <div className="pt-20 pb-24 lg:pb-10 mx-auto w-full flex justify-center items-center bg-gray-100 h-full">
-      <div className="w-full max-w-[1270px] flex gap-4  flex-col lg:flex-row ">
-        <div className="w-full flex flex-col-reverse lg:flex-col">
-          <section className="bg-white mx-2 xl:mx-0">
-            <p className="bg-gray-50 py-2 ps-3 text-sm font-medium">
-              Add Shipping Address
-            </p>
-            <form
-              onSubmit={handleSubmit(onSubmitOrder)}
-              className="px-3 py-2 flex flex-col gap-3"
-            >
-              <input
-                type="text"
-                id="fullname"
-                placeholder="Fullname: Mr. ABC"
-                {...register("fullname", {
-                  required: "Name is required",
-                })}
-                className="border text-sm py-2 ps-2 focus:border-secondary outline-none"
-              />
-              {errors.fullname && (
-                <p className="text-red-600 text-xs">
-                  {errors.fullname.message}
-                </p>
-              )}
-              <input
-                type="number"
-                id="phone"
-                placeholder="Phone Number: 012XXXXXXXX"
-                {...register("phone", {
-                  required: "Phone is required",
-                  minLength: {
-                    value: 11,
-                    message: "Enter a valid phone number",
-                  },
-                })}
-                className="border text-sm py-2 ps-2 focus:border-secondary outline-none"
-              />
-              {errors.phone && (
-                <p className="text-red-600 text-xs">{errors.phone.message}</p>
-              )}
-              <input
-                type="text"
-                id="email"
-                placeholder="Email: user@email.com"
-                {...register("email", {
-                  pattern: {
-                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-                    message: "Enter a valid email address",
-                  },
-                })}
-                className="border text-sm py-2 ps-2 focus:border-secondary outline-none"
-              />
-              {errors.email && (
-                <p className="text-red-600 text-xs">{errors.email.message}</p>
-              )}
-              <input
-                type="text"
-                placeholder="Address: House# 23, Street# 3, ABC road"
-                {...register("address", {
-                  required: "Address must required",
-                })}
-                className="border text-sm py-2 ps-2 focus:border-secondary outline-none"
-              />
-              {errors.address && (
-                <p className="text-red-600 text-xs">{errors.address.message}</p>
-              )}
-              <textarea
-                name="orderNote"
-                id="orderNote"
-                placeholder="Order note (optional)"
-                {...register("orderNote")}
-                className="border p-2 text-sm focus:border-secondary outline-none"
-              />
-            </form>
-          </section>
+    <div className="pt-20 pb-24 lg:pb-10 mx-auto w-full flex flex-col  items-center h-full">
+      <div className="w-full max-w-[1270px] ">
+        {/* Breadcrumb */}
+        <nav aria-label="breadcrumb">
+          <ol className="flex space-x-2 text-gray-400 text-sm">
+            <li>Home</li>
+            <li>{">"}</li>
+            <li className="font-medium text-black">Checkout</li>
+          </ol>
+        </nav>
 
-          {/* selected items */}
-          <section className="bg-white lg:mt-5 mb-5 p-3 mx-2 xl:mx-0">
-            {isLoading && <Loader size="30px" />}
-            {items?.map((item) => (
-              <div key={item._id} className="flex border-b pt-3 pb-3">
-                <img
-                  src={item.productImages[0]}
-                  alt="img"
-                  className="w-20 h-20"
+        {/* main content */}
+        <div className="flex flex-col lg:flex-row gap-4 lg:gap-12  justify-center mt-5">
+          <div className="w-full flex flex-col-reverse lg:flex-col">
+            {/* Shipping address */}
+            <section className="bg-white mx-2 xl:mx-0">
+              <p className="bg-gray-50 py-2 ps-3 text-sm font-semibold tracking-wide">
+                Shipping Address
+              </p>
+              <form
+                onSubmit={handleSubmit(onSubmitOrder)}
+                className="px- py-2 flex flex-col gap-3"
+              >
+                <input
+                  type="text"
+                  id="fullname"
+                  placeholder="Fullname: Mr. ABC"
+                  {...register("fullname", {
+                    required: "Name is required",
+                  })}
+                  className="border text-sm py-2 ps-2 focus:border-secondary outline-none"
                 />
-                <div className="ml-3">
-                  <p className="text-wrap">{item.productName}</p>
-                  <div className="flex gap-8 mt-6 text-sm">
-                    <p>
-                      Price: <span>&#2547;</span> {item.price}
-                    </p>
+                {errors.fullname && (
+                  <p className="text-red-600 text-xs">
+                    {errors.fullname.message}
+                  </p>
+                )}
+                <input
+                  type="number"
+                  id="phone"
+                  placeholder="Phone Number: 012XXXXXXXX"
+                  {...register("phone", {
+                    required: "Phone is required",
+                    minLength: {
+                      value: 11,
+                      message: "Enter a valid phone number",
+                    },
+                  })}
+                  className="border text-sm py-2 ps-2 focus:border-secondary outline-none"
+                />
+                {errors.phone && (
+                  <p className="text-red-600 text-xs">{errors.phone.message}</p>
+                )}
+                <input
+                  type="text"
+                  id="email"
+                  placeholder="Email: user@email.com"
+                  {...register("email", {
+                    pattern: {
+                      value:
+                        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                      message: "Enter a valid email address",
+                    },
+                  })}
+                  className="border text-sm py-2 ps-2 focus:border-secondary outline-none"
+                />
+                {errors.email && (
+                  <p className="text-red-600 text-xs">{errors.email.message}</p>
+                )}
+                <input
+                  type="text"
+                  placeholder="Address: House# 23, Street# 3, ABC road"
+                  {...register("address", {
+                    required: "Address must required",
+                  })}
+                  className="border text-sm py-2 ps-2 focus:border-secondary outline-none"
+                />
+                {errors.address && (
+                  <p className="text-red-600 text-xs">
+                    {errors.address.message}
+                  </p>
+                )}
+                <textarea
+                  name="orderNote"
+                  id="orderNote"
+                  placeholder="Order note (optional)"
+                  {...register("orderNote")}
+                  className="border p-2 text-sm focus:border-secondary outline-none"
+                />
+              </form>
+            </section>
 
-                    <p>Quantity: {item.quantity}</p>
+            {/* selected items */}
+            <section className="bg-white lg:mt-5 mb-5 p- mx-2 xl:mx-0">
+              {isLoading && <Loader size="30px" />}
+              {items?.map((item) => (
+                <div key={item._id} className="flex pt-3 pb-3 border-b">
+                  <img
+                    src={item.productImages[0]}
+                    alt="img"
+                    className="w-24 h-24 bg-gray-500"
+                  />
+                  <div className="ml-5">
+                    <p className="text-wrap">{item.productName}</p>
+                    <div className="flex gap-8 mt-12 text-sm">
+                      <p>
+                        Price: <span>&#2547;</span> {item.price}
+                      </p>
+
+                      <p>Quantity: {item.quantity}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </section>
-        </div>
+              ))}
+            </section>
 
-        {/* Order Summary */}
-        <div className="xl:w-[600px] bg-white p-3 mx-2 xl:mx-0 shadow-sm h-fit">
-          <section className="flex items-center justify-end">
-            <input
-              type="text"
-              className="border text-sm py-2 ps-2 w-80 outline-none focus:border-secondary"
-              placeholder="Coupon Code"
-            />
-            <button className="bg-secondary py-2 px-5 text-white ml-2 rounded">
-              Apply
-            </button>
-          </section>
-          <section className="mt-7">
-            <h1>Order Summary</h1>
-            <div className="flex justify-between items-center mt-3">
-              <p className="text-black/60 text-sm">Items Total</p>
-              <p>
-                <span>&#2547;</span> {itemsTotal}
-              </p>
-            </div>
-            <div className="flex justify-between items-center mt-3">
-              <p className="text-black/60 text-sm">Shipping Charge</p>
-              <p>
-                <span>&#2547;</span> {shippingCharge}
-              </p>
-            </div>
-            <div className=" flex justify-between items-center border-t mt-5 pt-3 px-1">
-              <p>Total</p>
-              <p className="text-secondary text-lg">
-                <span>&#2547;</span> {total}
-              </p>
-            </div>
-            <button
-              onClick={onSubmitOrder}
-              className="w-full bg-primary hover:bg-opacity-95 py-2 rounded-sm text-white mt-4"
-            >
-              Proceed to Pay
-            </button>
-          </section>
+            {/* payment methods */}
+            <section>
+              <h1 className="font-bold tracking-wide">Payment Methods</h1>
+            </section>
+          </div>
+
+          {/* Order Summary */}
+          <div className="xl:w-[500px] mx-2 xl:mx-0 h-fit">
+            <h1 className="font-medium mb-4">Order Summary</h1>
+            <section className="flex items-center justify-end">
+              <input
+                type="text"
+                className="border text-sm py-2 ps-4 w-80 outline-none rounded focus:border-black"
+                placeholder="Coupon Code"
+              />
+              <button className="py-2 px-5 ml-3 font-semibold border border-black/50 hover:outline hover:outline-1 text-sm rounded-full">
+                Apply
+              </button>
+            </section>
+            <section className="mt-7 border-t">
+              <div className="flex justify-between items-center mt-3 text-sm">
+                <p>Items Total</p>
+                <p>
+                  <span>&#2547;</span> {itemsTotal}
+                </p>
+              </div>
+              <div className="flex justify-between items-center mt-3 text-sm">
+                <p>Shipping Charge</p>
+                <p>
+                  <span>&#2547;</span> {shippingCharge}
+                </p>
+              </div>
+              <div className="flex justify-between items-center border-t mt-5 pt-3">
+                <p>Total</p>
+                <p className="text-lg">
+                  <span>&#2547;</span> {total}
+                </p>
+              </div>
+              <button
+                onClick={onSubmitOrder}
+                className="w-full bg-primary hover:bg-opacity-95 py-2 rounded-sm text-white mt-4"
+              >
+                Submit Order
+              </button>
+            </section>
+          </div>
         </div>
       </div>
     </div>
