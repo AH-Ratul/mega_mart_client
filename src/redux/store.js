491dbaba837d5f831e6b0b3c69d62c1fd06c209c
AH-Ratul <ahratul21@gmail.com>
2025-04-26 00:51:19 +0600

    update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,7 @@ import { productApi } from "./api/products_api";
 import quantityReducer from "./slices/quantitySlice";
 import cartReducer from "./slices/cartSlice";
 import { cartApi } from "./api/cart_api";
+import { contactApi } from "./api/contact_api";
 
 export const store = configureStore({
   reducer: {
@@ -16,12 +17,14 @@ export const store = configureStore({
     [userApi.reducerPath]: userApi.reducer,
     [productApi.reducerPath]: productApi.reducer,
     [cartApi.reducerPath]: cartApi.reducer,
+    [contactApi.reducerPath]: contactApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       userApi.middleware,
       productApi.middleware,
-      cartApi.middleware
+      cartApi.middleware,
+      contactApi.middleware
     ),
 });
