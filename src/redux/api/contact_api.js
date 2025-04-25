708589ecac6c642b99cfe9a1863d9b4c7d92c666
AH-Ratul <ahratul21@gmail.com>
2025-04-26 00:49:53 +0600

    manage api to handle add  contact

---
+++ src/redux/api/contact_api.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const contactApi = createApi({
+  reducerPath: "contactApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_baseurl}api/v1/`,
+    credentials: "include",
+  }),
+  tagTypes: ["contact"],
+  endpoints: (builder) => ({
+    addContact: builder.mutation({
+      query: ({ userId, ...data }) => ({
+        url: `contact/addContact/${userId}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useAddContactMutation } = contactApi;
