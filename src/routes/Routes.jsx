a6697f353d444802b5b0ce5bca6c5dc73e0eb47b
AH-Ratul <ahratul21@gmail.com>
2025-04-25 23:04:31 +0600

    new route added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -16,6 +16,7 @@ const VerifyCode = lazy(() => import("../pages/VerifyCode/VerifyCode"));
 const ProductDetails = lazy(() => import("../pages/ProductDetails/ProductDetails"));
 const CartPage = lazy(() => import("../pages/Cart/CartPage"));
 const Checkout = lazy(() => import("../pages/Checkout/Checkout"));
+const ContactInfo = lazy(() => import("../pages/Contact_Info/ContactInfo"));
 
 //Suspense wrapper 
 const loadable =(Component) => (
@@ -68,6 +69,14 @@ export const router = createBrowserRouter([
       {
         path: "shopping_cart",
         element: loadable(CartPage),
+      },
+      {
+        path: "contact_information",
+        element: ( 
+          <PrivateRoute>
+            {loadable(ContactInfo)}
+          </PrivateRoute>
+        ),
       },
       {
         path: "/checkout",
